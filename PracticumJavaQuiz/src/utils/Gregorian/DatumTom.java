9a6bb45f7592e0db9aaaeae2780af65e44e9ec18
inertom <tomwou@gmail.com>
2014-10-11 12:12:13 +0200

    Begin van Datum wrapper klasse Utils klasse gemaakt met enum van maanden in nl

---
+++ PracticumJavaQuiz/src/utils/Gregorian/DatumTom.java
@@ -0,0 +1,103 @@
+package utils.Gregorian;
+
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Calendar;
+
+import utils.Utils;
+import utils.Utils.MaandenVanHetJaar;
+
+
+public class DatumTom {
+
+	private GregorianCalendar datum;
+	
+	public DatumTom() {
+		this.datum = new GregorianCalendar();
+		this.datum.setTime(new Date());
+		
+	}
+	
+	public DatumTom(int dag, int maand, int jaar) {
+		this.datum = new GregorianCalendar(jaar,maand,dag);
+	}
+	
+	public DatumTom(String strDatum){
+		String[] array = strDatum.split("/");
+		this.datum = new GregorianCalendar(Integer.parseInt(array[2]),Integer.parseInt(array[1]),Integer.parseInt(array[0]));
+	}
+	
+	public int getDag() {
+		return this.datum.get(Calendar.DAY_OF_MONTH);
+	}
+	
+	public int getMaand() {
+		return this.datum.get(Calendar.MONTH);
+	}
+	
+	public int getJaar() {
+		return this.datum.get(Calendar.YEAR);
+	}
+	
+	public void setDag(int dag) {
+		this.datum.set(Calendar.DAY_OF_MONTH, dag);
+	}
+	
+	public void setMaand(int maand) {
+		this.datum.set(Calendar.MONTH, maand);
+	}
+	
+	public void setJaar(int jaar) {
+		this.datum.set(Calendar.YEAR, jaar);
+	}
+	
+	public String toString(){
+		return String.format("%02d %s %04d", this.getDag(), MaandenVanHetJaar.valueOf(String.valueOf(this.getMaand())), this.getJaar());
+		
+	}
+	
+	public String getDatumInAmerikaansFormaat() {
+		return String.format("%04d/%02d/%02d", this.getJaar(),this.getMaand(),this.getDag());
+	}
+	
+	public String getDatumInEuropeesFormaat() {
+		return String.format("%02d/%02d/%04d", this.getDag(),this.getMaand(),this.getJaar());
+	}
+	
+	public boolean equals(DatumTom datum) {
+		return (this.getDag()==datum.getDag()&&this.getMaand()==datum.getMaand()&&this.getJaar()==datum.getJaar()?true:false);
+		
+	}
+	
+	public
+	
+	public boolean kleinerDan(DatumTom datum){
+		if (this.getJaar() < datum.getJaar()){
+			return true;
+		}
+		else {
+			if (this.getJaar()> datum.getJaar()) {
+				return false;
+			}
+			else {
+				if (this.getMaand() < datum.getMaand()){
+					return true;
+					
+				}
+				else {
+					if (this.getMaand() > datum.getMaand()){
+						return false;
+					}
+					else {
+						if (this.getDag() < datum.getDag()) {
+							return true;
+						}
+						else {return false;}
+					}
+				}
+			}
+			
+		}
+		return false;
+	}
+}
---
+++ PracticumJavaQuiz/src/utils/Utils.java
@@ -0,0 +1,8 @@
+package utils;
+
+public class Utils {
+	public static enum MaandenVanHetJaar {nul,Januari,Februari,Maart,April,Mei,Juni,Juli,Augustus,September,Oktober,November,December};
+
+	
+
+}
