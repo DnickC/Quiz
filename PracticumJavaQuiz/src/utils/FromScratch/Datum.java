8dc8c8175a0f37adeffb7b0fec2de31fb448b13f
AndyPoron <AndyPoron@gmail.com>
2014-10-11 11:14:14 +0200

    New code baby
    Yej

--- PracticumJavaQuiz/src/utils/FromScratch/Datum.java
+++ PracticumJavaQuiz/src/utils/FromScratch/Datum.java
@@ -2,7 +2,7 @@ package utils.FromScratch;
 import java.util.Date;
 
 public class Datum {
-	private Date datumVandaag; 
+	private Date datumVandaag = new Date(); 
 	private int dag;
 	private int maand;
 	private int jaar;
@@ -14,7 +14,7 @@ public class Datum {
 		{ null, "januari", "februari", "maart", "april", "mei", "juni", "juli", "augustus", "september", "oktober", "november", "december" };
 	
 	public Datum(){
-		datumVandaag = new Date();
+		
 		dag = datumVandaag.getDay();
 		maand = datumVandaag.getMonth();
 		jaar = datumVandaag.getYear();
@@ -32,9 +32,10 @@ public class Datum {
 	}
 	
 	public Datum(String datum){
-		dag = datum.codePointCount(0, 1);
-		maand = datum.codePointCount(3, 4);
-		jaar = datum.codePointCount(5, 6); // 5 = " / " , dus 6 tot 9 ?
+		String[] splitdate = datum.split("/");
+		this.setDag(Integer.parseInt(splitdate[0]));
+		this.setMaand(Integer.parseInt(splitdate[1]));
+		this.setJaar( Integer.parseInt(splitdate[2]));
 	}
 		
 		/** setDatum method */
@@ -77,12 +78,12 @@ public class Datum {
 	}
 	
 	// methode om schrikkeldag te valideren
-	public int valideerDag (int dag)
+	public boolean valideerDag (Datum datum)
 	{
-		if (maand == 2 && dag == 29 && ((jaar % 4 == 0 && jaar % 100 != 0) || jaar % 400 == 0 )) {
-			return dag;			
+		if (datum.maand == 2 && datum.dag == 29 && ((datum.jaar % 4 == 0 && datum.jaar % 100 != 0) || datum.jaar % 400 == 0 )) {
+			return true;			
 		}
-		return 0;
+		return false;
 	}
 	
 	/** getDatumInAmerikaansFormaat method */
@@ -103,36 +104,102 @@ public class Datum {
 		return String.format("%02d/%s/%d", this.dag, naamVanMaand [this.maand], this.jaar);		
 	}
 	
-	public void equals (){
-		
+	public boolean equals (Datum datum){
+		//if(object date instanceOf Date){ indien het met alle objecten moet geregeld worden
+		if(this.dag == datum.dag && this.maand == datum.maand && this.jaar == datum.jaar){
+			return true;
+		}
+		return false;
 	}
 	
-	public void compareTo (){
+	public int compareTo (Datum datum){
+		if(this.dag == datum.dag && this.maand == datum.maand && this.jaar == datum.jaar){
+			return 0;
+		}
+		if(this.jaar < datum.jaar){
+			return -1;
+		}
+		if(this.jaar == datum.jaar && this.maand < datum.maand){
+			return -1;
+		}
+		if(this.jaar == datum.jaar && this.maand == datum.maand && this.dag < datum.dag){
+			return -1;
+		}
+		return 1;
 		
 	}
 	
 	/** kleinerDan method : bepaalt of een datum kleiner is dan huidig datumobject */
 	public boolean kleinerDan (Datum d){
+		if(compareTo(d) == -1 || compareTo(d) == 0){
+			return true;
+		}
 		return false;		
 	}
 	
 	public int verschilInJaren (Datum d){
-		return 0;
+		int dagen = verschilInDagen(d);
+		return dagen/365;  // floor? 
 	}
 	
 	public int verschilInMaanden (Datum d){
-		return 0;
+		int dagen = verschilInDagen(d);
+		return (dagen/365)*12;
 	}
 	
 	public int verschilInDagen (Datum d){
+		int vandaag = julianDayCalculator(this.jaar, this.maand,this.dag);
+		int dat = julianDayCalculator(d.jaar,d.maand,d.dag);
+		return vandaag-dat;
 		return 0;
 	}
 	
-	public void veranderDatum (int aantalDagen){
+	public void veranderDatum (Datum datum, int aantalDagen ){
+		
 		
 	}
 	
-	public Datum veranderDatum (int aantalDagen){
-		 
+	public Datum trekDagAf(Datum datum){
+		if(datum.maand == 
 	}
+	
+	public Datum voegDagToe(Datum datum){
+			if (datum.maand == 2){
+				if(datum.dag == 28){
+					if(valideerDag(datum)== true){
+						datum.dag++;
+					}else{
+						datum.dag = 1;
+						if(datum.maand == 12){
+						datum.maand = 1;
+						datum.jaar++;
+					}else{
+						datum.maand ++;
+					}
+				}
+			}
+			if(datum.dag == 30){
+				if(dagenPerMaand[datum.maand] == 30){
+					datum.dag = 1;
+					if(datum.maand == 12){
+						datum.maand = 1;
+						datum.jaar++;
+					}else{
+						datum.maand ++;
+					}
+				}else{
+					datum.dag++;
+				}
+			  }
+			}
+	}
+	
+	public int julianDayCalculator(int y,int m, int d){
+		y+= 8000;
+		if(m<3){y--;m+=12;}
+		return (y*365) +(y/4) -(y/100) +(y/400) -1200820
+	              +(m*153+3)/5-92
+	              +d-1;
+	}
+	
 }
