973f024308c79ea064bbf6ef41c231fa73cf6b72
MichalMytkowski <mmytkowski@gmail.com>
2014-10-11 09:13:07 +0200

    Small Update 1

--- PracticumJavaQuiz/src/utils/Gregorian/Datum.java
+++ PracticumJavaQuiz/src/utils/Gregorian/Datum.java
@@ -1,25 +1,39 @@
 package utils.Gregorian;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Calendar; // Bron : http://stackoverflow.com/questions/9115897/how-do-i-convert-a-java-sql-date-object-into-a-gregoriancalendar
 import java.util.GregorianCalendar;
 
 public class Datum {
-	
-	private GregorianCalendar datumVandaag;
+	private SimpleDateFormat amerikaansDatumFormaat = new SimpleDateFormat("yyyy/MM/dd");
+	private SimpleDateFormat europeesDatumFormaat = new SimpleDateFormat("dd/MM/yyyy");
+	private SimpleDateFormat volledigeDatum = new SimpleDateFormat("dd/MMM/yyyy");
+	private Calendar datumVandaag = new GregorianCalendar();;
 	private int dag;
 	private int maand;
 	private int jaar;
 	
+	private static final int[] dagenPerMaand = 
+		{ 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	
+	private static final String[] naamVanMaand = 
+		{ null, "januari", "februari", "maart", "april", "mei", "juni", "juli", "augustus", "september", "oktober", "november", "december" };
 	
 	//Constructor zonder param
 	public Datum ()
 	{
-		datumVandaag = new GregorianCalendar();
-		dag = datumVandaag.DAY_OF_MONTH;
-		maand = datumVandaag.MONTH;
-		jaar = datumVandaag.YEAR;
 		
+		dag = Calendar.DAY_OF_MONTH;
+		maand = Calendar.MONTH;
+		jaar = Calendar.YEAR;
+		
+		//oorspronkelijk -- gaf melding dat het beter static kon worden gemaakt:
+		/*
+		 * dag = datumVandaag.DAY_OF_MONTH;
+		 * maand = datumVandaag.MONTH;
+		 * jaar = datumVandaag.YEAR;
+		 */
 	}
 	
 	
@@ -46,16 +60,60 @@ public class Datum {
 	}
 
 	/**Set methoden:*/
-	public final void SetDatum(Date datum)
+	public void setJaar(int jaar)
 	{
-		datumVandaag.setTime(datum);
+		if(jaar >= 0)
+		{
+			datumVandaag.set(Calendar.YEAR, jaar);
+		}
+		else
+		{
+			throw new IllegalArgumentException("Ongeldig jaartal");
+		}
 	}
 	
+	public void setMaand(int maand)
+	{
+		if (maand < 12 && maand > 1 )
+		{
+			datumVandaag.set(Calendar.MONTH, maand);
+		}
+		else
+		{
+			throw new IllegalArgumentException("Ongeldige maand");
+			
+		}
+	}
+	
+	public void setDag(int dag)
+	{
+		
+	}
+	
+	
+	public String getDatumInAmerikaansFormaat()
+	{
+				
+		return amerikaansDatumFormaat.format(datumVandaag.getTime());
+	}
+	
+	public String getDatumInEuropeesFormaat()
+	{
+		return europeesDatumFormaat.format(datumVandaag.getTime());
+	}
+
+public String toString()
+{
+	return volledigeDatum.format(datumVandaag.getTime());
+}
+
 	//Methode schrikkeljaar
 	public boolean schrikkeljaar(int jaar){
-		boolean isLeapYear = datumVandaag.isLeapYear(datumVandaag.YEAR);
+		boolean isLeapYear = ((GregorianCalendar) datumVandaag).isLeapYear(datumVandaag.YEAR);
 		return isLeapYear;
 		
 	}
+	
+	
 	
 }
