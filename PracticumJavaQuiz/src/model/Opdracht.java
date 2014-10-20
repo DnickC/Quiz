129df9e8b405d8f514b36becedf41e6dad1d3814
AndyPoron <AndyPoron@gmail.com>
2014-10-20 20:43:35 +0200

    syncdedddd

--- PracticumJavaQuiz/src/model/Opdracht.java
+++ PracticumJavaQuiz/src/model/Opdracht.java
@@ -1,5 +1,22 @@
 package model;
 
+import java.util.*;
+
 public class Opdracht {
 
+	private String vraag = null;
+	private String antwoord = null;
+	private int maxAantalPogingen = 0;
+	private List<String> antwoordHints = new ArrayList<String>();
+	private int antwoordTijd = 0;
+	
+	public void Opdracht(String vraag, String antwoord, int maxAantalPogingen, int maxAntwoordTijd){
+		this.vraag = vraag;
+		this.antwoord = antwoord;
+		this.maxAantalPogingen = maxAantalPogingen;
+		this.antwoordTijd = maxAntwoordTijd;	
+	}
+	
+	
+	
 }
