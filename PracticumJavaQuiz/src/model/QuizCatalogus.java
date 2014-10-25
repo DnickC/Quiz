3b97efc1c4732d6004d4b97f9bb2e81e81620c2c
jensvankets <jensvankets@hotmail.com>
2014-10-25 11:10:49 +0200

    QuizCatalogus

---
+++ PracticumJavaQuiz/src/model/QuizCatalogus.java
@@ -0,0 +1,40 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuizCatalogus {
+	
+	private List<Quiz> quizzes = new ArrayList<>();
+	
+	public QuizCatalogus(List<Quiz> quizes)
+	{
+		this.setQuizzes(quizzes);
+	}
+	
+	public List<Quiz> getQuizzes()
+	{
+		return quizzes;
+	}
+	
+	public void setQuizzes()
+	{
+		this.quizzes = quizzes;
+	}
+	
+	public void addToe(Quiz quiz)
+	{
+		quizzes.add(quiz);
+	}
+	
+	public void alterQuiz(Quiz quiz)
+	{
+		// methodes om quiz aan te passen???
+	}
+	
+	public void deleteQuiz(Quiz quiz)
+	{
+		quizzes.remove(quiz);
+	}
+	
+}
