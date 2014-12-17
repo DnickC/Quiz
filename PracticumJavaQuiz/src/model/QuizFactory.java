5c60101cd6f36a96b2297d787b050068cad99a67
jensvankets <jensvankets@hotmail.com>
2014-12-17 19:03:26 +0100

    I'M BACK BITCHES

--- PracticumJavaQuiz/src/model/QuizFactory.java
+++ PracticumJavaQuiz/src/model/QuizFactory.java
@@ -4,6 +4,11 @@ import model.Quiz.QuizStatus;
 
 public class QuizFactory{
 	
+	/**
+	 * Authors: Jens van Kets
+	 * Version : 
+	 */
+	
 	public Quiz getQuiz(String onderwerp, int leerjaar, boolean isTest,boolean uniekeDeelname,Leraar leraar, QuizStatus status) {
 		
 		return new Quiz(onderwerp, leerjaar, isTest, uniekeDeelname, leraar, status);
